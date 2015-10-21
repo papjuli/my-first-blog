525f490fe647be5589ccdca288159fb1587991f0
Julia Pap <papjuli@info.ilab.sztaki.hu>
2015-10-21 22:08:31 +0200

    python version agnostic number generator test

--- blog/numbers.py
+++ blog/numbers.py
@@ -11,8 +11,11 @@ if __name__ == "__main__":
   import sys
   up_to = int(sys.argv[1])
   while True:
-    n = numbers.next()
+    if sys.version_info[0] <= 2:
+      n = numbers.next()
+    else:
+      n = numbers.__next__()
     if n > up_to:
       break
     else:
-      print n
+      print (n)
